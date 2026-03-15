b80fcbbdcbf6107ede3527ff9a3c74ed5c0fa14b
wasonisgood <43530227+wasonisgood@users.noreply.github.com>
2026-03-15 23:16:14 +0800

    更更新

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 修正 GitHub Pages 404 問題
   server: {
     // 禁用 HMR 覆蓋層以避免某些進程衝突
     hmr: {
