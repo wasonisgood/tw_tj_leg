b51ecb0dfb1cf1f408cd52b014b43e3308ecdfe6
wasonisgood <43530227+wasonisgood@users.noreply.github.com>
2026-03-16 00:00:13 +0800

    更新路境

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 修正 GitHub Pages 404 問題
+  base: '/tw_tj_leg/', // 明確設定 GitHub Pages 儲存庫名稱
   server: {
-    // 禁用 HMR 覆蓋層以避免某些進程衝突
     hmr: {
       overlay: false
     }
   },
   optimizeDeps: {
-    // 強制重新預構建依賴
     force: true
   }
 })
